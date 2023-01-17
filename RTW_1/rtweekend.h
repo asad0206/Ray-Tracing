b1beec4ded5d4855c60837877bc5938a445beda9
Asad <asad200ana@gmail.com>
2023-01-17 23:48:03 +0530

    added camera class multi sampled color support and ANTI ALIASING

--- RTW_1/rtweekend.h
+++ RTW_1/rtweekend.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -25,6 +26,38 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // returns a random real number in range (0,1)
+    return rand() / (RAND_MAX + 1.0);
+    // RAND_MAX > 32767 always
+    // and dividing by a huge number is better cuz we get more varied
+    // decimals even at small incremets of the denominator
+}
+
+// if we want to use full c++ to get random numbers this is cpp code
+/*inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}*/
+
+inline double random_double(double min, double max)
+{
+    // returns a random real number in (min, max)
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
 }
 
 // Common Headers
